4d8a1a0b73ad0e5dc1067e3c9f26551fdbde847c
Justin Mello <justinmello@u.boisestate.edu>
2024-11-17 18:26:54 -0700

    missing commit re-adding

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 window.onload = function() {
     insertNavbar();
+    detectActivePage();
 };
 
 function insertNavbar() {
+    /* Create the nav bar */
     const navHtml =`
         <nav>
-          <a href="../pages/index.html">Home</a>
-          <a href="../pages/login.html">Login</a>
-          <a href="../pages/index.html">Dashboard</a>
-          <a href="../pages/login.html">Transact</a>
-          <a href="../pages/index.html">Download</a>
-          <a href="../pages/login.html">Contact</a>
+            <ul>
+                <li id="index"><a href="../pages/index.html">Home</a></li>
+                <li id="login" ><a href="../pages/login.html">Login</a></li>
+                <li id="dashboard"><a href="../pages/dashboard.html">Dashboard</a></li>
+                <li id="transact"><a href="../pages/transact.html">Transact</a></li>
+                <li id="download"><a  href="../pages/download.html">Download</a></li>
+                <li id="contact" ><a href="../pages/contact.html">Contact</a></li>
+            </ul>
         </nav>
     `;
 
+    /* Insert the nav bar into the header and footer */
     const header = document.querySelector('header');
     const footer = document.querySelector('footer');
     if (header){
@@ -30,8 +35,20 @@ function insertNavbar() {
 }
 
 function detectActivePage(){
+    /* Get the current page */
     currentPage =window.location.pathname;
-    console.log(currentPage);
+
+    /* Split the path and get the last element */
+    currentPage = currentPage.split("/");
+    currentPage = currentPage[currentPage.length - 1];
+    currentPage = currentPage.split(".");
+    currentPage = currentPage[0];
+
+    /* Get the nav links for the current page*/
+    const navLinks = document.querySelectorAll(`#${currentPage}`);
+    navLinks.forEach(link => {
+        link.classList.add('active');
+    });
 }
 
 
